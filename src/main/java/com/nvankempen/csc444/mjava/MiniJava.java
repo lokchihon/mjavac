b2ccf7bd18a5dd9f27c50644cfb1b1c9f28bae12
nicovank <nvankemp@gmail.com>
2018-10-11 13:10:18 -0400

    Got rid of Listener Parser.

--- src/main/java/com/nvankempen/csc444/mjava/MiniJava.java
+++ src/main/java/com/nvankempen/csc444/mjava/MiniJava.java
@@ -1,29 +1,24 @@
 package com.nvankempen.csc444.mjava;
 
-import com.nvankempen.csc444.mjava.ast.nodes.Program;
-import com.nvankempen.csc444.mjava.parser.ListenerOrientedParser;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
 
 public class MiniJava {
 
     public static void main(String[] args) throws IOException {
-        ListenerOrientedParser parser = new ListenerOrientedParser();
-        Program p = parser.parse(CharStreams.fromStream(System.in));
 
-        System.out.println("ok");
+        MiniJavaParser parser = new MiniJavaParser(new CommonTokenStream(new MiniJavaLexer(CharStreams.fromStream(System.in))));
 
-        // https://jakubdziworski.github.io/java/2016/04/01/antlr_visitor_vs_listener.html
+        ErrorListener errorListener = new ErrorListener();
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
 
-//        ErrorListener errorListener = new ErrorListener();
-//        parser.removeErrorListeners();
-//        parser.addErrorListener(errorListener);
-//
-//        ParseTree tree = parser.program();
-//
-//        if (!errorListener.hasSyntaxErrors()) {
-//            System.out.println(tree.toStringTree(parser));
-//        }
+        ParseTree tree = parser.program();
+
+        if (!errorListener.hasSyntaxErrors()) {
+            System.out.println(tree.toStringTree(parser));
+        }
     }
 }
